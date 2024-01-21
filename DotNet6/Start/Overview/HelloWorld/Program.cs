339811df5a2d8f3f76539a76f024609c961a59cc
jptran0 <jazminetra00@gmail.com>
2024-01-20 21:59:22 -0600

    finished hello world assignment

--- DotNet6/Start/Overview/HelloWorld/Program.cs
+++ DotNet6/Start/Overview/HelloWorld/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there " + str);
