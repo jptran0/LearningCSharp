d1461ed1d1bcebc214b784bc2c3f8f061cf7229e
jptran0 <jazminetra00@gmail.com>
2024-01-23 19:02:25 -0600

    completed defining properties

--- DotNet6/Start/Classes/Modifiers/obj/Debug/net6.0/Modifiers.AssemblyInfo.cs
+++ DotNet6/Start/Classes/Modifiers/obj/Debug/net6.0/Modifiers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modifiers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a421db7c691233b49cab6e8fe1531ea051d846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f7f739b375a39b84574fe0e7ae1def1cf5feaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modifiers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modifiers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
